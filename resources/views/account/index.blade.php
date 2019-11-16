e3ea8743e53c50d7f0b003e0216a6ee68f2a486c
Tim Joosten <Tjoosten@users.noreply.github.com>
2019-11-16 10:45:18 +0100

    Delete index.blade.php

--- resources/views/account/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('account.layouts.default')
-
-@section('account.content')
-    <div class="card card-default">
-        <div class="card-body">
-            Account overview
-        </div>
-    </div>
-@endsection
